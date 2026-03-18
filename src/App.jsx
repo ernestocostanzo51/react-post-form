4759c6e288b25cadd2a7e652f4352cf34cbdf751
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-18 16:51:33 +0100

    inserimento value e onchange tramite function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import { useEffect } from 'react'
 function App() {
 
 const [post, usePost] = useState({
-  
+  name: "",
+  title: "",
+  body: "",
+  public: false
 })
 
   const link = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
@@ -17,44 +20,82 @@ const [post, usePost] = useState({
     })
   }, [])
 
+  function handleChange(e) {
+    const { name, value, type } = e.target.value;
+    setPost({...post,[name]: type === 'radio' ? (value === 'true') : value
+    });
+  }
 
 
   return (
     <>
      <div className='container'>
+      <form>
       <div className='row'>
           <div className='col-6 m-3'>
             <p>Autore:</p>
-           <input type="text" class="form-control" id="exampleFormControlInput1" placeholder='inserisci autore'></input>
+           <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder='inserisci autore'
+            name='name'
+            value={post.name}
+            onChange={handleChange}>
+            </input>
            </div>
           <div className='col m-3'>
             <p>Titolo:</p>
-           <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="inserisci titolo"></input>
+           <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="inserisci titolo"
+            name='title'
+            value={post.title}
+            onChange={handleChange}>
+            </input>
           </div>
         </div>
         <div className='row'>
           <div className='col-8'>
           <p>inserisci il testo</p>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <textarea
+           class="form-control"
+           id="exampleFormControlTextarea1"
+           rows="3"
+           value={post.body}
+           onChange={handleChange}>
+          </textarea>
           </div>
-          <div className='col-2'>
+          <div className='col-1'>
             <p>Privato</p>
-              <input class="form-check-input" type="radio"></input>
+              <input
+               class="form-check-input"
+               type="radio"
+               name="public" 
+               value="false" 
+               checked={post.public === false}
+               onChange={handleChange}>
+               </input>
               </div>
-              <div className='col-2'>
+              <div className='col-1'>
                 <p>Publico</p>
-            <input class="form-check-input" type="radio"></input>
+            <input
+             class="form-check-input"
+              type="radio"
+              name="public" 
+              value="false" 
+              checked={post.public === false}
+              onChange={handleChange}>
+
+              </input>
               </div>
-             
-
-          
+              <div className='col'>
+                <button type='submit' className='btn btn-primary'>Posta</button>
+              </div>
         </div>
-
-          
-        
-        
-    
-          
+</form>
       </div>
      
     </>
